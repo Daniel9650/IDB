4039a738d1becf3490a4fa6e313cd2de9efbbb9b
danieltalamas <daniel.talamas@utexas.edu>
2018-02-10 15:47:40 -0600

    Changed TopMovies.py to be in Python 3

--- TopMovies.py
+++ TopMovies.py
@@ -2,43 +2,42 @@
 Gets top 3 most popular movies and prints the info we want from them
 """
 
-import urllib2
-import json
+import requests
+
 
 # read genres
-genres = urllib2.urlopen("https://api.themoviedb.org/3/genre/movie/list?api_key=21fed2c614e1de3b61f64b89beb692a5&language=en-US").read()
-genres = json.loads(genres)
+genres = requests.get(
+    "https://api.themoviedb.org/3/genre/movie/list?api_key=21fed2c614e1de3b61f64b89beb692a5&language=en-US").json()
 
-# convert genres to a dictionary with key genre_id and value of the string name of the genre
+# convert genres to a dictionary with key genre_id and value of the string
+# name of the genre
 genres_dic = {}
 genres = genres["genres"]
 for gen in genres:
     genres_dic[gen["id"]] = gen["name"]
 
 # read first page of popular movies
-content = urllib2.urlopen("https://api.themoviedb.org/3/discover/movie?api_key=21fed2c614e1de3b61f64b89beb692a5&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1").read()
-
-movies = json.loads(content)
+movies = requests.get(
+    "https://api.themoviedb.org/3/discover/movie?api_key=21fed2c614e1de3b61f64b89beb692a5&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1").json()
 movies = movies["results"]
 
-print
+print()
 for i in range(3):
     movie = movies[i]
-    print (i+1),"Movie name:", movie["original_title"]
-    print "Overview:", movie["overview"]
-    print "Release date:", movie["release_date"]
-    print "Poster path: http://image.tmdb.org/t/p/w185" + movie["poster_path"]
+    print ((i + 1), "Movie name:", movie["original_title"])
+    print ("Overview:", movie["overview"])
+    print ("Release date:", movie["release_date"])
+    print (
+        "Poster path: http://image.tmdb.org/t/p/w185" +
+        movie["poster_path"])
     topics = movie["genre_ids"]
-    print "Topics:",
+    print ("Topics:", end=' ')
 
     for e in range(len(topics)):
         t = topics[e]
         first = False
         if e < len(topics) - 1:
-            print str(genres_dic[t]) + ",",
+            print (str(genres_dic[t]) + ",", end=' ')
         else:
-            print genres_dic[t],
-
-    print "\n"
-
-print
+            print (genres_dic[t])
+    print()
