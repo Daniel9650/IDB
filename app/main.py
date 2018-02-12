3d020d686ae5ac4a431531648feebffe10af10ba
kyliesanderson <kyliesanderson@utexas.edu>
2018-02-12 17:10:22 -0600

    tring send_from_directory in python script

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
 
 # the all-important app variable:
-app = Flask(__name__)
+app = Flask(__name__, static_folder='build')
 
 @app.route("/")
 def hello():
-    return render_template("index.html");
+    return send_from_directory('build', 'index.html');
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
