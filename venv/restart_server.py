f46da4cf463929130e79cd8927b64732a7879ca4
Daniel Olmos <olmos@cs.utexas.edu>
2018-02-13 12:52:45 -0600

    Added server restart script

---
+++ venv/restart_server.py
@@ -0,0 +1,16 @@
+from subprocess import Popen
+
+Popen(["source","venv","/bin/activate"])
+Popen(["sudo", "docker", "build", "-t", "poptopic_app"])
+Popen(["sudo","docker", "ps", ">>", "temp.txt"])
+
+# Find last line of file
+file = open("temp.txt")
+file_line_list = file.readlines()
+file.close()
+name = file_line_list[-1]
+
+Popen(["rm", "temp.txt"])
+Popen(["sudo", "docker", "kill ", name])
+Popen(["sudo", "docker", "run", "-d", "--restart=always", "-p80:80", "-t", "poptopic_app"])
+
