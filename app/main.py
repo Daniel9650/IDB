02c97f53abfcd7dc4842e2e0e50669ac371a8675
Drew Dearing <ddearing@cs.utexas.edu>
2018-02-12 18:56:00 -0600

    add more main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,23 @@
+import os
 from flask import Flask, send_from_directory
 
 # the all-important app variable:
 app = Flask(__name__, static_folder='build')
 
-@app.route("/")
-def hello():
-    return send_from_directory('build', 'index.html');
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def hello(path):
+    if(path == ""):
+        return send_from_directory('build', 'index.html')
+    else:
+        s = path.split("/")
+        fname = s[len(s) - 1]
+        del s[len(s) - 1]
+        s.insert(0, "build")
+        if(os.path.exists("build/" + path)):
+            return send_from_directory(os.path.join(*s), fname)
+        else:
+            return send_from_directory('build', 'index.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='0.0.0.0', debug=True, port=80, use_reloader=True, threaded=True)
