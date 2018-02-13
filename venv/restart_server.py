aaf0e2c6d93724fccdf8d7de4a373de03c1dca88
Daniel Olmos <olmos@cs.utexas.edu>
2018-02-13 13:46:05 -0600

    More bug fixes

--- venv/restart_server.py
+++ venv/restart_server.py
@@ -2,11 +2,11 @@ from subprocess import Popen
 
 Popen(["sudo", "touch", "temp"])
 Popen(["sudo", "docker", "build", "-t", "poptopic_app","."])
-Popen(["sudo","docker", "ps", ">>", "temp"])
+Popen(["sudo","docker", "ps", ">", "temp"])
 
 # Find last line of file
 with open("temp", "r") as file:
-name = file.readlines()[-1]
+	name = file.readlines()[-1]
 
 Popen(["rm", "temp"])
 Popen(["sudo", "docker", "kill ", name])
