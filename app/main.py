dbb250cd7f2413cf4e49d66b94d3723e1e7f0a14
Drew Dearing <ddearing@cs.utexas.edu>
2018-02-13 13:21:02 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -10,14 +10,31 @@ def hello(path):
     if(path == ""):
         return send_from_directory('build', 'index.html')
     else:
-        s = path.split("/")
-        fname = s[len(s) - 1]
-        del s[len(s) - 1]
-        s.insert(0, "build")
         if(os.path.exists("build/" + path)):
-            return send_from_directory(os.path.join(*s), fname)
+            return send_from_directory('build/', path)
         else:
             return send_from_directory('build', 'index.html')
+
+@app.route('/static/css/<path:path>')
+def static_css(path):
+    if(os.path.exists("build/" + path)):
+        return send_from_directory('build/static/css/', path)
+    else:
+        return send_from_directory('build', 'index.html')
+
+@app.route('/static/js/<path:path>')
+def static_js(path):
+    if(os.path.exists("build/" + path)):
+        return send_from_directory('build/static/js/', path)
+    else:
+        return send_from_directory('build', 'index.html')
+
+@app.route('/static/media/<path:path>')
+def static_media(path):
+    if(os.path.exists("build/" + path)):
+        return send_from_directory('build/static/media/', path)
+    else:
+        return send_from_directory('build', 'index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80, use_reloader=True, threaded=True)
