1cc70d7b8f2e5366f51a637865b7eff3d644f8b5
Drew Dearing <ddearing@cs.utexas.edu>
2018-02-13 14:09:51 -0600

    adding js static route

--- app/main.py
+++ app/main.py
@@ -14,5 +14,10 @@ def static_proxy(path):
   # send_static_file will guess the correct MIME type
   return app.send_static_file('build/'+ path)
 
+  @app.route('/static/js/<path:path>')
+def static_js(path):
+  # send_static_file will guess the correct MIME type
+  return app.send_static_file('build/static/js'+ path)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80, use_reloader=True, threaded=True)
