42b2f8edf170dfb7f2676174c49080a9d1656cb4
Daniel Olmos <olmos@cs.utexas.edu>
2018-02-13 13:55:18 -0600

    Server restart script updates

--- venv/restart_server.py
+++ venv/restart_server.py
@@ -1,14 +1,17 @@
 from subprocess import Popen
 
-Popen(["sudo", "touch", "temp"])
+Popen(["sudo", "touch", "temp.out"])
 Popen(["sudo", "docker", "build", "-t", "poptopic_app","."])
-Popen(["sudo","docker", "ps", ">", "temp"])
+Popen(["sudo","docker", "ps", ">", "temp.out"])
+print("FINISHED DOCKER PS")
 
 # Find last line of file
-with open("temp", "r") as file:
+with open("temp.out", "r") as file:
 	name = file.readlines()[-1]
 
-Popen(["rm", "temp"])
+print("GOT NAME: ", name)
+Popen(["rm", "temp.out"])
 Popen(["sudo", "docker", "kill ", name])
 Popen(["sudo", "docker", "run", "-d", "--restart=always", "-p80:80", "-t", "poptopic_app"])
-
+print("DONE")
+exit()
