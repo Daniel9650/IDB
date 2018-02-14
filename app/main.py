63c28c505bc2bf4616fb0e4829822327d92f933d
Drew Dearing <drewdearing@Drews-MacBook-Pro.local>
2018-02-14 04:16:06 -0600

    working flask template site

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, send_from_directory, render_template
+from flask import Flask, request, send_from_directory, render_template, url_for
 
 # the all-important app variable:
 app = Flask(__name__)
@@ -9,35 +9,43 @@ app = Flask(__name__)
 # Splash page
 @app.route('/')
 def index():
-    return "index"
+    return render_template("index.html")
 
 #Page for browsing models (must include a grid, links to individual instances)
 
-@app.route('/songs')
+@app.route('/music')
 def song_catalog():
-    return "song_catalog"
+    return render_template("model.html", name="Music")
 
 @app.route('/books')
 def book_catalog():
-    return "book_catalog"
+    return render_template("model.html", name="Books")
 
 @app.route('/movies')
 def movie_catalog():
-    return "movie_catalog"
+    return render_template("model.html", name="Movies")
+
+@app.route('/topics')
+def topic_catalog():
+    return render_template("model.html", name="Topics")
 
 #Pages for individual data points (must include embedded images, data, links)
 
-@app.route('/songs/<song_name>')
+@app.route('/music/<song_name>')
 def song(song_name):
-    return "song_name"
+    return render_template("model_song.html", name=song_name);
 
 @app.route('/books/<book_name>')
 def book(book_name):
-    return "book_name"
+    return render_template("model_book.html", name=book_name);
 
 @app.route('/movies/<movie_name>')
 def movie(movie_name):
-    return "movie_name"
+    return render_template("model_movie.html", name=movie_name);
+
+@app.route('/topics/<topic_name>')
+def topic(topic_name):
+    return render_template("model_topic.html", name=topic_name);
 
 if __name__ == "__main__":
     app.run()
