47847fe44df62853f869ed2fb423208413e238d8
kyliesanderson <kyliesanderson@utexas.edu>
2018-02-21 15:51:28 -0600

    deleted old react app and created new one

---
+++ react-app/src/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+
+
+class Home extends Component {
+   render () {
+      return (
+         <div>
+            <p>Hello there</p>
+            <Button>Sup</Button>
+         </div>
+      );
+   }
+
+}
+
+export default Home;
