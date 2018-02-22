902c7f790ea3a8d571b26025112fe15a951312e2
drewdearing <social@drewdearing.com>
2018-02-22 14:29:00 -0600

    start MovieInstance

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Home from './Home.js';
+import MovieInstance from './MovieInstance.js';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <MovieInstance />
     );
   }
 }
