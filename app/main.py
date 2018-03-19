8c000a137387f0c8bab31adaee419099d7de569d
kyliesanderson <kyliesanderson@utexas.edu>
2018-03-19 16:23:16 -0500

    edited main.py to display build

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ from flask import Flask, request, send_from_directory, render_template, url_for
 from GitInfo import get_commits_count, get_issues_count
 
 # the all-important app variable:
-app = Flask(__name__, template_folder='public', static_folder='src')
+app = Flask(__name__, template_folder='build', static_folder='build/static')
 
 # Splash page
 @app.route('/')
-def index(): 
+def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
