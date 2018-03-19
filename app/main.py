ac3c4cd2f6abb68be0e331ba3d4fe3caac7e6888
Drew Dearing <ddearing@cs.utexas.edu>
2018-03-19 17:05:46 -0500

    Path

--- app/main.py
+++ app/main.py
@@ -6,8 +6,8 @@ from GitInfo import get_commits_count, get_issues_count
 app = Flask(__name__, template_folder='build', static_folder='build/static')
 
 # Splash page
-@app.route('/')
-def index():
+@app.route('/<path:path>')
+def index(path):
     return render_template("index.html") 
 
 if __name__ == "__main__":
