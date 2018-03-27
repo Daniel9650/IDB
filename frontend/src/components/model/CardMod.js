2150c6837a8a36d4ce29bdaec081ccf57b6408d9
drewdearing <social@drewdearing.com>
2018-03-27 17:23:02 -0500

    fix

--- frontend/src/components/model/CardMod.js
+++ frontend/src/components/model/CardMod.js
@@ -10,14 +10,15 @@ import { Card,
 let CardMod = function(props) {
    var image = props.image;
    var title = props.title;
-
+   var model_type = props.type;
    var name = "card" + props.number;
    var id =  props.id;
+   var url_model = "/" + model_type.toLowerCase() + "/" + id;
 
    if(props.type != "Topics") {
       var topics = props.topics.join(", ");
       return (
-         <a name={name} href={id}>
+         <a name={name} href={url_model}>
             <Card>
                <CardImg top src={image} />
                <CardBody>
@@ -29,7 +30,7 @@ let CardMod = function(props) {
    }
    else {
       return (
-         <a name={name} href={props.id}>
+         <a name={name} href={url_model}>
             <Card>
                <CardImg top src={image} />
                <CardBody>
