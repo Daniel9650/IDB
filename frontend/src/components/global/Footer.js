bebd2e3223709b4a2b046eeda830fe81f535bc3d
drewdearing <social@drewdearing.com>
2018-04-05 20:35:46 -0500

    Footer css

--- frontend/src/components/global/Footer.js
+++ frontend/src/components/global/Footer.js
@@ -4,9 +4,15 @@ class Footer extends Component {
 
    render(){
       return(
-         <div className="footer">
-            <p>Double Daniel Inc.</p>
+         <footer className="footer">
+         <div className="container">
+         <p className="float-right">
+         <a href="#">Back to top</a>
+         </p>
+         <p>Copyright © 2018 Double Daniel, Inc.</p>
+         <p>New to PopTopic? <a href="http://api.poptopic.org">Visit our API Documentation</a> or read our <a href="https://daniel9650.gitbooks.io/idb-phase-1/content/">technical report</a>.</p>
          </div>
+         </footer>
       );
    }
 }
